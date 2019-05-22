2b96da6eecc8ee26247c45220886286d978adef8
Yooleg <JJJuliusz@gmail.com>
2019-05-22 16:05:28 +0200

    Update

--- SearchingCurses/SearchingCurses/Program.cs
+++ SearchingCurses/SearchingCurses/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,24 @@ namespace SearchingCurses
     {
         static void Main(string[] args)
         {
+            var songLyrics = new SongLyrycs(artist:"Shakira", title:"Nada");
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+
+    class SongLyrycs
+    {
+        private string artist;
+        private string title;
+
+        public SongLyrycs(string artist, string title)
+        {
+            var browser = new WebClient();
+            var url = "https://api.lyrics.ovh/v1/" + artist + "/" + title;
+            var json = browser.DownloadString(url);
+            Console.WriteLine(json);
         }
     }
 }
+
