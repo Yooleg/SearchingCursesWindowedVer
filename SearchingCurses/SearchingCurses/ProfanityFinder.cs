770f5a6254d36fd83e99d4b61116cec301dafba3
Yooleg <JJJuliusz@gmail.com>
2019-05-31 09:50:35 +0200

    more progress

---
+++ SearchingCurses/SearchingCurses/ProfanityFinder.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SearchingCurses
+{
+    internal class ProfanityFinder
+    {
+        private string[] badWords;
+
+        public ProfanityFinder()
+        {
+            var dictFile = File.ReadAllText("../profanities.txt");
+            dictFile = dictFile.Replace("*","");
+            dictFile = dictFile.Replace("(", "");
+            dictFile = dictFile.Replace(")", "");
+            badWords = dictFile.Split(new []{"\",\""}, StringSplitOptions.None);
+            
+        }
+
+        public string Censore(string text)
+        {
+            foreach (var word in badWords)
+            {
+                text = RemoveBadWord(text, word);
+            }
+            return text;
+        }
+
+  
+
+        public int CountBadWords(string text)
+        { 
+            var vulgarAmount = 0;
+        
+            foreach (var word in badWords)
+            {
+                if (text.Contains(word))
+                    vulgarAmount = vulgarAmount + 1;
+             
+            }
+            return vulgarAmount;
+        }
+
+        public Dictionary<string, int> FindTopBadWords(string lyrics)
+        {
+            var dict = new Dictionary<string, int>();
+               
+            foreach (var word in badWords)
+            {
+                int occurrancesOfWord = CalcOccurances(word, lyrics);
+                if (occurrancesOfWord > 0)
+                    dict[word] = occurrancesOfWord;
+            }
+            return dict;
+        }
+
+        int CalcOccurances(string word, string lyrics)
+        {
+            return Regex.Matches(word, lyrics).Count;
+        }
+
+        public string GetBadWordsSummary(SongLyrycs song)
+        {
+            var summary = "";
+
+            var badWordsAmount = FindTopBadWords(song.lyrics);
+
+                foreach (var sth in badWordsAmount)
+            {
+                summary = summary + "\n" + sth.Key + " - " + sth.Value;
+            }
+
+                return summary;
+        }
+
+        private static string RemoveBadWord(string text, string word)
+        {
+            var pattern = "\\b" + word + "\\b";
+            return Regex.Replace(text, pattern, "___", RegexOptions.IgnoreCase);
+            
+        }
+    }
+}
+
--- SearchingCurses/SearchingCurses/Program.cs
+++ SearchingCurses/SearchingCurses/Program.cs
@@ -1,57 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SearchingCurses
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var songLyrics = new SongLyrycs(artist:"Eminem", title:"Without me");
             var profoanityFinder = new ProfanityFinder();
-            
-            var censored = profoanityFinder.Censore(songLyrics.lyrics);
 
-            Console.WriteLine(censored);
+            // var censored = profoanityFinder.Censore(songLyrics.lyrics);
+            var vulgarAmount = profoanityFinder.CountBadWords(songLyrics.lyrics);
+            Console.WriteLine(vulgarAmount);
+            var dict = profoanityFinder.FindTopBadWords(songLyrics.lyrics);
+            Console.WriteLine(profoanityFinder.GetBadWordsSummary(songLyrics));
+            //Console.WriteLine(censored);
 
             Console.WriteLine("Done.");
             Console.ReadLine();
-        }
-    }
-
-    internal class ProfanityFinder
-    {
-        private string[] badWords;
-
-        public ProfanityFinder()
-        {
-            var dictFile = File.ReadAllText("../profanities.txt");
-            dictFile = dictFile.Replace("*","");
-            dictFile = dictFile.Replace("(", "");
-            dictFile = dictFile.Replace(")", "");
-            badWords = dictFile.Split(new []{"\",\""}, StringSplitOptions.None);
-            
-        }
-
-        public string Censore(string text)
-        {
-            foreach (var word in badWords)
-            {
-                text = RemoveBadWord(text, word);
-            }
-            return text;
-        }
-
-        private static string RemoveBadWord(string text, string word)
-        {
-            var pattern = "\\b" + word + "\\b";
-            return Regex.Replace(text, pattern, "___", RegexOptions.IgnoreCase);
-            
         }
     }
 }
